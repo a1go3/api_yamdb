2f5b904926229bdba3e236e5cf0522c2facfb60d
tatarenkov-r-v <flexomj@gmail.com>
2023-05-15 18:09:41 +0400

    Добавлен ReviewSerializer

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from reviews.models import Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(slug_field='username',
+                                          read_only=True)
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('title',)
